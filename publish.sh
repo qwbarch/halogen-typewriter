3a6dfa569de9643016d4bd108905c67a50fae14a
qwbarch <qwbarch@gmail.com>
2023-07-16 02:26:12 -0400

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
-git checkout main
-git branch gh-pages
-git checkout gh-pages
-git rm -r .
-git checkout main -- ./public/*
-git commit -am "Update github pages"
-git push origin gh-pages
-git checkout main
+if ! git diff-index --quiet HEAD --; then
+  echo "There are uncommitted changes. Please commit or stash them before running the script."
+  exit 1
+fi
+
+if [ -z "$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)" ]; then
+  echo "The current branch is not tracking a remote branch. Please push your changes first."
+  exit 1
+fi
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$current_branch" = "main" ]; then
+  if git rev-parse --quiet --verify "gh-pages" >/dev/null; then
+    git checkout gh-pages
+  else
+    git checkout --orphan gh-pages
+  fi
+
+  mv public/index.js index.js
+  rm -r public
+  find . -mindepth 1 ! -regex "^./.git(/.*)?" -name "index.js" -delete
+  git checkout main -- ./public/*
+  git add .
+  git commit -m "Update github pages"
+  git push origin gh-pages
+  git checkout main
+
+  echo "Succesfully pushed to github-pages!"
+else
+  echo "Please switch to the main branch before publishing to github pages"
+fi
